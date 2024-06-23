061de582f7c3cfd50c7c9c40aeacc5349c90f118
EkagraAgarwal08 <ekagraagarwal01@gmail.com>
2024-06-24 02:16:01 +0530

    adding bodyparser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const bodyparser = require('body-parser');
+
+
 const { port } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended:true}));
     // create an express object
     const app = express();
     app.listen(port, () => {
