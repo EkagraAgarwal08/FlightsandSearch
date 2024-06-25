1b1b607a596dfd51d37bbf6ebc76fae7c574373f
EkagraAgarwal08 <ekagraagarwal01@gmail.com>
2024-06-25 12:27:31 +0530

    added first database

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const bodyparser = require('body-parser');
 
-
 const { port } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
-
-    app.use(bodyparser.json());
-    app.use(bodyparser.urlencoded({extended:true}));
     // create an express object
     const app = express();
+
+    // Middleware setup
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
+
+    // Start the server
     app.listen(port, () => {
         console.log(`Server started at port ${port}`);
     });
