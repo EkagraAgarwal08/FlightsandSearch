f9e1d98e7134736df11f5885506e6b3844333068
EkagraAgarwal08 <ekagraagarwal01@gmail.com>
2024-06-25 19:56:28 +0530

    completed city-repository.js

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -2,26 +2,51 @@ const {City} = require('../models/index');
 
 
 class CityRepository {
-  async CreateCity({name}){
-    try {
-        const city = await City.create({name});
-        return city;
+    async CreateCity({ name }) { 
+        try {
+            const city = await City.create({
+                name // it means name:name first name means the column in the database 2nd name means the value
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
     }
-    catch(error){
-     throw {error};
+    async DeleteCity(cityId) {
+        try {
+            await City.destroy({
+                where: {
+                    id: cityId
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
     }
-  } 
-  async DeleteCity(cityId){
-    try {
-        await City.destroy({
-            where:{
-                id:cityId
-            }
-        });
+    async GetCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
     }
-    catch(error){
-     throw {error};
+    async UpdateCity(cityId,data){
+        try {
+            const city = await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            })
+            
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
     }
-  }
 }
 module.exports = CityRepository;
