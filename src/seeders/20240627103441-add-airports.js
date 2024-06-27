6073895d7368201dfbbb60761913e2997e99c91c
EkagraAgarwal08 <ekagraagarwal01@gmail.com>
2024-06-27 16:54:52 +0530

    added seeds

---
+++ src/seeders/20240627103441-add-airports.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { name } = require('body-parser');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Airports',[
+      {
+        name:'Indira Gandhi International Airport',
+        cityId:11,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        name:'Safdarjung Airport',
+        cityId:11,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        name:'Chhatrapati Shivaji International Airport',
+        cityId:12,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        name:'Pune International Airport',
+        cityId:12,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
