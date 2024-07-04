cf485892b2a4a95eaf5a999b1f146834fcde8360
EkagraAgarwal08 <ekagraagarwal01@gmail.com>
2024-07-05 00:11:38 +0530

    added errors

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -1,5 +1,5 @@
 // middlewares/flight-middleware.js
-
+const { ClientErrorCodes } = require('../utils/error-codes');
 const validateCreateFlight = (req, res, next) => {
     console.log('validateCreateFlight middleware called');
     if(
@@ -13,7 +13,7 @@ const validateCreateFlight = (req, res, next) => {
     ) {
         // if any of the body params is missing we come inside the if
         console.log('Validation failed: Missing mandatory properties');
-        return res.status(400).json({
+        return res.status(ClientErrorCodes.BAD_REQUEST).json({
             data: {},
             success: false,
             message: 'Invalid request body for create flight',
